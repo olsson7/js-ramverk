6f483d697e84ff191350a31607cbd80191d9d3a6
olsson7 <72479962+olsson7@users.noreply.github.com>
2023-05-11 19:34:57 +0200

    installed bootstrap into project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button type="button" class="btn btn-success">Success</button>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
