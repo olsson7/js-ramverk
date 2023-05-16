8e5aea2da681459afbb2d3c84e91f78d98cf996b
olsson7 <72479962+olsson7@users.noreply.github.com>
2023-05-16 10:00:51 +0200

    fix app.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import "./App.css"
 function App() {
   return (
     <div>
-      <header>
         <h1>Min filmlista</h1>
         <h2>Lägg till en film</h2>
         <hr></hr>
         <AddMovieForm/>
 
-      </header>
     </div>
   );
 }
